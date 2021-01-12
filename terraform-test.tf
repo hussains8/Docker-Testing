e59be96869723c495a2838c0710e2c5441c03ae4
hussains8 <66360797+hussains8@users.noreply.github.com>
2021-01-12 16:35:53 +0500

    Update terraform-test.tf

--- terraform-test.tf
+++ terraform-test.tf
@@ -1,1 +1,6 @@
-ss
+resource "null_resource" "cluster" {
+    provisioner "local-exec" {  
+        command = "echo test > completed.txt"  
+        interpreter = ["PowerShell", "-Command"]  
+    }  
+} 
